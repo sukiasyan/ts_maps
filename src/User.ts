90583608fb78789b56f2e04d865827afec4f0ef7
hsukiasyan <h.sukiasyan@gethatch.com>
2021-08-01 19:35:06 +0200

    add markers and popups

--- src/User.ts
+++ src/User.ts
@@ -10,10 +10,12 @@ export class User {
   constructor() {
     this.name = faker.name.firstName();
     this.location = {
-        lat: parseFloat(faker.address.latitude()),
-        lng: parseFloat(faker.address.longitude()),
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
   }
 }
-
-
- 
